21571219b9b90a8ead6455e99a5d84d34316db9a
Frederico Martins <fredericomartins@outlook.com>
2016-12-30 11:32:04 +0000

    Update centreon-orchestration.py

--- centreon-orchestration.py
+++ centreon-orchestration.py
@@ -4,27 +4,56 @@ __author__ = "Frederico Martins"
 __license__ = "GPLv3"
 __version__ = 0.1
 
-from argparse import ArgumentParser, SUPPRESS, RawDescriptionHelpFormatter
-from configparser import ConfigParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from configparser import ConfigParser, MissingSectionHeaderError
 from getpass import getpass
 from os import path
 from subprocess import PIPE, Popen
-from sys import argv
 from textwrap import dedent
 
 
-pool = {
-    'alfr0101vmh':'10.2.38.10',
-    'alfr0102vmh':'10.2.38.11',
-    'alfr0103vmh':'10.2.38.12'
-}
+default = dedent('''\
+    host_action = add
+    host_alias = Host Name
+    host_ip = 10.1.20.1
+    host_template = templatename
+    host_group = hostgroupname
+    host_resource = aclresourcename
+    host_poller = pollername
+    host_snmp_community = 1ft4Ko9uqdt8U
+    host_snmp_version = 2c
+    hostgroup_action = add
+    hostgroup_alias = Hostgroup Name
+    hostgroup_service = servicename
+    hostgroup_resource = aclresourcename
+    service_action = add
+    service_alias = Service Name
+    service_template = templatename
+    resource_action = edit
+    poller_action = restart''')
+
+defaultconf = {param.split(' = ')[0]:param.split(' = ')[1] for param in default.split('\n')}
 
 
 class Parsing(object):
 
     name = path.basename(__file__)
+    objects = {'host':[], 'hostgroup':[], 'service':[], 'resource':[], 'poller':[]}
 
     def __init__(self):
+
+        self.Arguments()
+
+        if self.args.generate_ini and (not path.exists(self.args.ini) or input("File {0} exists, overwrite? [y/N] ".format(self.args.ini)) == 'y'):
+            self.Generate()
+
+        if self.args.generate_ini:
+            raise SystemExit
+
+        self.Configuration()
+
+
+    def Arguments(self):
 
         parser = ArgumentParser(prog='python {0}'.format(self.name), add_help=False,
             formatter_class=RawDescriptionHelpFormatter, description=dedent('''\
@@ -39,30 +68,114 @@ class Parsing(object):
 
         optional = parser.add_argument_group('Optional')
         optional.add_argument('-i', '--ini', metavar='file', type=str, help='Specify .ini configuration file.', required=True)
-        optional.add_argument('-g', '--generate-ini', action='store_true', help='Generate .ini configuration template file.')
         optional.add_argument('-u', '--user', metavar='user', type=str, help='Centreon user with administration privileges.',
             default='admin')
+        optional.add_argument('-g', '--generate-ini', action='store_true', help='Generate .ini template configuration file.')
         optional.add_argument('-h', '--help', action='help', help='Show this help message')
         optional.add_argument('-v', '--version', action='version', version='{0} {1}'.format(self.name, __version__),
             help='Show program version')
 
         self.args = parser.parse_args()
-            
+  
 
-class CentreonAPI(object):
+    def Generate(self):
+
+        with open(self.args.ini, 'w') as openini:
+            openini.write(dedent('''\
+                # Centreon .ini template, for environment orchestration.
+                # Clone each object based on it's type.
+
+                #Default program configurations
+                [default] 
+                {0}
+
+                [hostname]
+                type = host
+                #action = add # Uses the default 'host_action' if omitted
+                #alias = Host Name # Uses the default 'host_alias' if omitted
+                #ip = 10.1.20.1 # Uses the default 'host_ip' if omitted
+                #template = templatename # Uses the default 'host_template' if omitted
+                #group = hostgroupname # Uses the default 'host_group' if omitted
+                #resource = aclresourcename
+                #poller = mypoller # Uses the default 'host_poller' if omitted
+                #snmp_community = 1ft4Ko9uqdt8U
+                #snmp_version = 2c
+
+                [hostgroupname]
+                type = hostgroup
+                #action = add # Uses the default 'hostgroup_action' if omitted
+                #alias = Hostgroup Name
+                #service = name;template
+                #resource = aclresourcename
+
+                [servicename]
+                type = service
+                #action = add
+                #alias = Service Name
+                #template = templatename
+                #check_command = check-host-alive # check_host_alive = yes
+
+                [aclresourcename]
+                type = resource
+                #action = edit
+
+                [pollername]
+                type = poller
+                #action = restart''').format(default))
+
+
+    def Configuration(self):
+
+        print("Info: Parsing file '{0}'".format(self.args.ini))
+
+        try:
+            config = ConfigParser()
+            config.read(self.args.ini)
+
+        except MissingSectionHeaderError as e:
+            print("Error: {0}".format(e))
+            raise SystemExit
+
+        try: # Update default configurations, based on .ini file values
+            for key, value in config['default'].items():
+                defaultconf[key] = value.split('#')[0].strip()
+
+            del config['default']
+
+        except KeyError: # In case there is no Default section
+            pass
+
+        try:
+            for each in config.sections():
+                self.objects[config[each]['type']].append(config[each])
+
+        except KeyError as e:
+            print("Error: Missing {0} in '{1}'".format(e, each)) # "No such type {0}"
+            raise SystemExit
+
+
+class Orchestrate(object):
 
     group = 'VMWare'
     poller = 'Poller_SAS_OaM'
     resource = 'GSS_SAS-Resources'
     service = 'host-alive'
 
-    def __init__(self, user):
+    def __init__(self, user, objects):
 
-        self.user = user
+        self.user, self.objects = user, objects
 
-        self.Authenticate()
-        self.CreateService()
+        #self.Authenticate()
+
+        #for each in ['service', 'hostgroup', 'host']:
+        for service in self.objects['service']:
+            self.Service(service)
+
         self.CreateGroup()
+
+        # New
+        #for host in self.objects['host']:
+            #self.Host(host)
 
         for host, ip in pool.items():
             self.CreateHost(host, ip)
@@ -77,14 +190,23 @@ class CentreonAPI(object):
             shell=True, stdout=PIPE, stderr=PIPE).communicate()
 
         if stdout.strip():
-            print('Wrong username or password.')
+            print("Wrong username or password.")
             self.Authenticate()
 
 
-    def CreateService(self):
+    def Service(self, service):
 
-        self.Command('-o STPL -a add -v "{0};VMWare_Host_Alive;GSS-SAS-Generico"'.format(self.service))
-        self.Command('-o STPL -a setparam -v "{0};check_command;check-host-alive"'.format(self.service))
+        service = Assert(service)
+        name = str(service).partition(': ')[2][:-1]
+
+        if service['action'] == 'add':
+            self.Command('-o STPL -a add -v "{0};{1};{2}"'.format(name, service['alias'], service['template']))
+
+        
+#    def CreateService(self):
+
+        #self.Command('-o STPL -a add -v "{0};VMWare_Host_Alive;GSS-SAS-Generico"'.format(self.service))
+        #self.Command('-o STPL -a setparam -v "{0};check_command;check-host-alive"'.format(self.service))
 
 
     def CreateGroup(self):
@@ -127,92 +249,23 @@ class CentreonAPI(object):
         Popen('/usr/bin/centreon -u {0} -p {1} {2}'.format(self.user, self.password, command), shell=True).communicate()
 
 
-class CentreonINI():
+def Assert(object): # Asserts the default values, in case any attribute is not specified
+    
+    for param in defaultconf:
+        try:
+            if param.startswith('service'):
+                type = param.partition('_')[2]
+                object[type] = object[type].split('#')[0].strip() # Remove comment from value 
 
-    def __init__(self, filename, generate):
+        except KeyError:
+            object[type] = defaultconf[param]
 
-        if generate and (not path.exists(filename) or input("File {0} exists, overwrite? [y/N] ".format(filename)) == 'y'):
-            self.Generate(filename)
-
-        if generate:
-            raise SystemExit
-
-        self.Parse()
-
-
-    def Generate(self, filename):
-
-        with open(filename, 'w') as openini:
-            openini.write(dedent('''\
-                # Centreon .ini template, for environment orchestration.
-                # Clone each object based on it's type.
-
-                #Default program configurations
-                #[default] 
-                #host_action = add # add, edit, delete
-                #host_template = templatename
-                #host_group = hostgroupname
-                #host_resource = aclresourcename
-                #host_poller = pollername
-                #host_snmp_community = 1ft4Ko9uqdt8U
-                #host_snmp_version = 2c
-                #hostgroup_action = add
-                #hostgroup_resource = aclresourcename
-                #service_action = add
-                #service_template = templatename
-                #resource_action = edit
-                #poller_action = restart # restart, reload
-
-                [hostname]
-                type = host
-                alias = Host Name
-                ip = 10.1.20.1
-                #action = add # Uses the default 'host_action' if omitted
-                #template = templatename # Uses the default 'host_template' if omitted
-                #group = hostgroupname # Uses the default 'host_group' if omitted
-                #resource = aclresourcename
-                #poller = mypoller # Uses the default 'host_poller' if omitted
-                #snmp_community = 1ft4Ko9uqdt8U
-                #snmp_version = 2c
-
-                [hostgroupname]
-                type = hostgroup
-                alias = Hostgroup Name
-                service = name;template
-                #action = add # Uses the default 'hostgroup_action' if omitted
-                #resource = aclresourcename
-
-                [servicename]
-                type = service
-                alias = Service Name
-                #action = add
-                #template = templatename
-                #check_command = check-host-alive # check_host_alive = yes
-
-                [aclresourcename]
-                type = resource
-                #action = edit
-
-                [pollername]
-                type = poller
-                #action = restart'''))
-
-
-    def Parse(self):
-
-        objects = {'host':[], 'hostgroup':[], 'service':[], 'resource':[], 'poller':[]}
-
-        config = ConfigParser()
-        config.read('centreon.ini')
-
-        for each in config.sections():
-            objects[config[each]['type']].append(each)
+    return object
 
 
 try:
     p = Parsing()
-    CentreonINI(p.args.ini, p.args.generate_ini)
-    CentreonAPI(p.args.user)
+    Orchestrate(p.args.user, p.objects)
 
 except KeyboardInterrupt:
     raise SystemExit
