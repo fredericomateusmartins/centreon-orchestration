b50305cd5cafe996539eae55f147262736b84a66
Frederico Martins <22522942+fredericomateusmartins@users.noreply.github.com>
2022-02-04 16:45:44 +0000

    Update check_certificate_expiration.py

--- plugins/check_certificate_expiration.py
+++ plugins/check_certificate_expiration.py
@@ -27,7 +27,7 @@ class Parsing(object):
                   certificate expiration
                   check.
                 '''), epilog = dedent('''\
-                Check the git repository at https://github.com/flippym/nagios-collection,
+                Check the git repository at https://github.com/fredericomateusmartins/nagios-collection,
                 for more information about usage, documentation and bug report.'''))
 
         optional = parser.add_argument_group('Optional')
